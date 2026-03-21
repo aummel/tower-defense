0cb365a28e15d682927a9114991532b05695884b
aummel <chamel.saiyan1@gmail.com>
2026-03-21 19:11:09 -0400

    Initial commit

---
+++ Global/Game.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var gold = 100
+var health = 5
---
+++ Scenes/enemy_1.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+@export var speed = 150
+var Health = 10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+	if get_parent().get_progress_ratio() == 1:
+		queue_free()
+		Game.health -= 1
+		
+		
+	if Health <= 0:
+		get_parent().get_parent().queue_free()
+		Game.gold += 15
